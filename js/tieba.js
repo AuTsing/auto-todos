e389782371f376c71dc215ad2a15f4546c3abaae
AuTsing <763025696@qq.com>
2021-04-16 18:28:47 +0800

    add tieba

---
+++ js/tieba.js
@@ -0,0 +1,64 @@
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const download = require('download');
+const sct = require('./sct');
+
+const cookie = process.env.TIEBA_COOKIE;
+const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/BDTieBa-DailyBonus/TieBa.js';
+const sctKey = process.env.SCT_KEY;
+
+const dir = './tmp/';
+const jsFile = 'TieBa.js';
+const resultFile = 'result.txt';
+const errFile = 'err.txt';
+
+async function prepare() {
+    await download(url, dir);
+}
+
+function generate() {
+    if (!cookie) {
+        throw new Error('未填写COOKIE');
+    }
+    let content = fs.readFileSync(`${dir}${jsFile}`, 'utf8');
+    content = content.replace(/var cookieVal = \$nobyda.read("CookieTB");/, `var cookieVal = '${cookie}'`);
+    fs.writeFileSync(`${dir}${jsFile}`, content, 'utf8');
+}
+
+function run() {
+    execSync(`node ${dir}${jsFile} >> ${dir}${resultFile}`);
+}
+
+function report() {
+    if (sctKey) {
+        const title = '百度贴吧';
+        let content = '无执行结果';
+
+        if (fs.existsSync(`${dir}${resultFile}`)) {
+            content = fs.readFileSync(`${dir}${resultFile}`, 'utf8');
+        }
+
+        console.log(content);
+        // sct.send(title, content);
+    }
+}
+
+async function main() {
+    try {
+        await prepare();
+        console.log('下载完成');
+
+        generate();
+        console.log('配置完成');
+
+        run();
+        console.log('执行完成');
+
+        report();
+        console.log('汇报完成');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
